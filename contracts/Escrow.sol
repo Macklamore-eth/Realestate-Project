1ebbc58aaf4e9b43ae3cc3e4aaa0dc24c5f5a6fb
Mackenzie <Mackenzie.mavx@gmail.com>
2023-04-13 12:32:37 +0200

    Second Commit

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -10,19 +10,39 @@ contract Escrow {
 
     address public nftAddress;
     uint public nftID;
+    uint256 public purchasePrice;
+    uint256 public escrowAmount;
     address payable public seller;
     address payable public buyer;
+    address public inspector;
+    address public lender;
 
     constructor(
         address _nftAddress,
         uint256 _nftID,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount,
         address payable _seller,
-        address payable _buyer
+        address payable _buyer,
+        address _inspector,
+        address _lender
     ) {
         nftAddress = _nftAddress;
         nftID = _nftID;
+        purchasePrice = _purchasePrice;
+        escrowAmount = _escrowAmount;
         seller = _seller;
         buyer = _buyer;
+        inspector = _inspector;
+        lender = _lender;
+    }
+
+    function depositEarnest() public payable {
+        require(msg.value >= escrowAmount);
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
     }
 
     function finaliseSale() public {
--- test/realestate.js
+++ test/realestate.js
@@ -1,10 +1,18 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+const ether = tokens
+
 describe('realestate', () => {
     let realestate, escrow
     let deployer, seller
     let nftID = 1
+    let purchasePrice = ether(100)
+    let escrowAmount = ether(20)
 
     beforeEach(async() => {
         //Set up accounts
@@ -12,6 +20,8 @@ describe('realestate', () => {
         deployer = accounts[0]
         seller = deployer
         buyer = accounts[1]
+        inspector = accounts[2]
+        lender = accounts[3]
 
 
         //Load contracts
@@ -22,8 +32,13 @@ describe('realestate', () => {
         escrow = await Escrow.deploy(
             realestate.address,
             nftID,
+            purchasePrice,
+            escrowAmount,
             seller.address,
-            buyer.address
+            buyer.address,
+            inspector.address,
+            lender.address
+
         )
 
         //Seller Approves NFT
@@ -38,14 +53,25 @@ describe('realestate', () => {
     })
 
     describe('Selling real estate', async () => {
+        let balance, transaction
         it('executes a successul transaction', async () => {
+
+            //Expects seller to be NFT owner before the sale
             expect(await realestate.ownerOf(nftID)).to.equal(seller.address)
+
+            //Buyer deposits earnest
+            transaction = await escrow.connect(buyer).depositEarnest({ value: ether(20) })
+
+            //check escrow balance
+            balance = await escrow.getBalance()
+            console.log('escrow balance', ethers.utils.formatEther(balance))
 
             //finalize sale
             transaction = await escrow.connect(buyer).finaliseSale()
             await transaction.wait()
             console.log("Buyer finalises sale")
 
+            //Expects buyer to be NFT owner after the sale
             expect(await realestate.ownerOf(nftID)).to.equal(buyer.address)
         })
     })
